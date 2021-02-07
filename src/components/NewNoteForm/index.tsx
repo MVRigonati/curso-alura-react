459801a0d838ac6f013e006d7363473e1d5b858a
= <=>
2021-02-07 18:39:30 -0300

    Adding category to the form

--- src/components/NewNoteForm/index.tsx
+++ src/components/NewNoteForm/index.tsx
@@ -1,47 +1,44 @@
-import React, { ChangeEvent, FormEvent, useState, Dispatch } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { FormEvent, Dispatch, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { addNote } from '../../store/action/notes';
-import { MyAction } from '../../store/store';
+import { MyAction, MyStore } from '../../store/store';
 import { Note } from "../Note";
 
 import './NewNoteForm.css';
 
 const NewNoteForm: React.FC = () => {
 
-	const [title, setTitle] = useState('');
-	const [text, setText] = useState('');
+	const categoryRef = useRef<HTMLSelectElement>(null);
+	const textRef = useRef<HTMLTextAreaElement>(null);
+	const titleRef = useRef<HTMLInputElement>(null);
+	
 	const dispatch = useDispatch<Dispatch<MyAction>>();
-
-	const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
-		event.stopPropagation();
-		setTitle(event.target.value);
-	}
-
-	const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-		event.stopPropagation();
-		setText(event.target.value);
-	}
+	const categories = useSelector<MyStore, string[]>(s => s.categories);
 
 	const createNote = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		event.stopPropagation();
-		dispatch(addNote(new Note(title, text)));
+
+		dispatch(addNote(new Note(titleRef.current!.value, textRef.current!.value, categoryRef.current!.value)));
 	}
 
 	return (
 		<form className="new-note-form" onSubmit={createNote}>
+			<select className="new-note-form_input" ref={categoryRef}>
+				<option></option>
+				{categories.map((cat, index) => <option key={index}>{cat}</option>)}
+			</select>
+
 			<input type="text"
-				placeholder="Título"
-				value={title}
-				className="new-note-form_input"
-				onChange={handleTitleChange} />
+				placeholder="Title"
+				ref={titleRef}
+				className="new-note-form_input" />
 
 			<textarea rows={15}
-				placeholder="Escreva sua nota..."
-				value={text}
-				className="new-note-form_input"
-				onChange={handleTextChange} />
+				placeholder="Write your note..."
+				ref={textRef}
+				className="new-note-form_input" />
 
 			<button className="new-note-form_submit">Criar Nota</button>
 		</form>
--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -9,10 +9,12 @@ import "./Note.css";
 export class Note {
     public title: string;
     public text: string;
+    public category: string;
 
-    constructor(title: string, text: string) {
+    constructor(title: string, text: string, category: string) {
         this.title = title;
         this.text = text;
+        this.category = category;
     }
 }
 
@@ -28,6 +30,7 @@ const NoteView: React.FC<Props> = (props) => {
             <header className="note_header">
                 <h3 className="note_text">{props.note.title}</h3>
                 <DeleteImage onClick={() => NotesListStore.dispatch(removeNote(props.index))} />
+                <h4>{props.note.category}</h4>
             </header>
             <p className="note_title">{props.note.text}</p>
         </section>
